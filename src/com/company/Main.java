ba7195ff6c812bbb17b240f031e06363833ad7e1
amir <khamidullin.amir@gmail.com>
2022-02-03 22:12:34 +0500

    Hometask for 05.02 / Java Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,33 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int s =0;
+        int s = 0;
+        int k = 0;
+        int a = 0;
 
         int n = scanner.nextInt();
         int[] integers = new int[n];
         for(int i = 0; i < n; i++){
             integers[i] = scanner.nextInt();
-        }
-        for(int i = 0; i < n-1; i++){
-            if (integers[i+1] > integers[i]){
 
-                s++;
-            }
         }
 
-        System.out.println(s);
+        for(int i = 0; i < n-2; i++){
+            k = integers[i];
+            integers[i] = integers[i+2];
+            integers[i+2] = k;
+
+        }
+
+        System.out.println(Arrays.toString(integers));
+
+
+
 
     }
 }
